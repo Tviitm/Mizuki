ea3786a241a07267274d9420df60c5bcb6861f40
Tviitm <sch3547057196@outlook.com>
2025-12-22 20:46:37 +0800

    修改网站前缀

--- src/config.ts
+++ src/config.ts
@@ -23,7 +23,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "百月歌",
 	subtitle: "BlOG",
-	siteURL: "www.cwayin.top/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://cwayin.top/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-12-22", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
